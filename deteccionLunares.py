752b31ac6c23a5bc0c10fbcd8eeacc5531028c37
48592151 <sofiamossa12@gmail.com>
2024-11-26 11:14:24 -0300

    mejoras

--- deteccionLunares.py
+++ deteccionLunares.py
@@ -14,9 +14,7 @@ model_path = "melanoma1.h5"
 
 model = load_model(model_path)
 
-def predict_image(image_path):
-    img = cv2.imread(image_path)
-    img = cv2.resize(img, (64, 64))
+def predict_image(img):
     img = np.expand_dims(img, axis = 0)
     img = img / 255.0
     prediction = model.predict(img)
@@ -27,17 +25,14 @@ def predict_image(image_path):
 
 @app.post("/predict/")
 async def predict(file: UploadFile = File ('...')):
-    img_path = f"temp_{file.file_name}"
-    with open (img_path, "wb") as f:
-        f.write(await file.read())
+    img = file.file
 
-        prob_benigno, prob_maligno = predict_image(img_path)
-        os.remove(img_path)
+    prob_benigno, prob_maligno = predict_image(img)
 
-        return {
-            "Probabilidad de que sea benigno": f"{prob_benigno * 100:.2f}%",
-            "Probabilidad de que sea maligno": f"{prob_maligno * 100:.2f}%"
-        }
+    return {
+        "Probabilidad de que sea benigno": f"{prob_benigno * 100:.2f}%",
+        "Probabilidad de que sea maligno": f"{prob_maligno * 100:.2f}%"
+    }
 
 # probar en render
 if __name__ == '__main__':
